1caab1eecb1ad38c3dc8f7ca5348c655d4bc308e
seoyun333 <jsy05031448@gmail.com>
2026-03-26 15:52:54 +0900

    Add files via upload

---
+++ listS.h
@@ -0,0 +1,40 @@
+// listS.h 내용
+#ifndef LISTS_H
+#define LISTS_H
+
+#define MAX 10
+
+// 함수의 원형(Prototype) 선언
+int insertElement(int L[], int size, int x);
+int deleteElement(int L[], int size, int x);
+
+// 삽입 함수 구현
+int insertElement(int L[], int size, int x) {
+    int i, move = 0;
+    for (i = 0; i < size; i++) {
+        if (L[i] > x) break;
+    }
+    for (int j = size; j > i; j--) {
+        L[j] = L[j - 1];
+        move++;
+    }
+    L[i] = x;
+    return move;
+}
+
+// 삭제 함수 구현
+int deleteElement(int L[], int size, int x) {
+    int i, move = 0;
+    for (i = 0; i < size; i++) {
+        if (L[i] == x) break;
+    }
+    if (i == size) return size;
+
+    for (int j = i; j < size - 1; j++) {
+        L[j] = L[j + 1];
+        move++;
+    }
+    return move;
+}
+
+#endif
